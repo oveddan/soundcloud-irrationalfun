a595e788e3e3b6f213cbadee5d82bd32915fbeff
oveddan <stangogh@gmail.com>
2014-05-03 03:07:01 -0400

    started specing out closest sequences, and built a basic max heap from the algorithms 4 course

--- index.js
+++ index.js
@@ -4,54 +4,23 @@ var readStream = fs.createReadStream('pi-billion.txt', {
   encoding: 'utf-8',
   autoClose: true,
   start: 2,
-  end: 100
+  end: 1000000
 });
 
 var Transform = require('stream').Transform;
 
-var characterStreamer = new Transform();
-characterStreamer._transform = function(chunk, encoding, next) {
+var sequenceStreamer = new Transform();
+sequenceStreamer._transform = function(chunk, encoding, next) {
   var result = [];
   // console.log(data.toString());
   var dataString = chunk.toString();
 
-  for(var i = 0; i < dataString.length; i++){
-    console.log('pushing: ');
-    console.log(dataString[i]);
-    this.push(dataString[i]);
+  for(var i = 0; i < dataString.length - 84; i++){
+    
+    this.push(dataString.slice(i, i + 84));
   }
+
   next();
 };
 
-characterStreamer.on('error', function(e, err){
-  console.log(e);
-});
-
-var Writable = require('stream').Writable;
-
-var sequenceStreamer = new Transform({ objectMode: true });
-
-sequenceStreamer._transform = function(chunk, encoding, next){
-  var sequence = [];
-  for(var i = 0; i < 84; i++) {
-    console.log('pulling: ');
-    console.log(parseInt(chunk.toString()));
-    sequence[i] = parseInt(chunk.toString());
-    next();
-  }
-  this.push(sequence);
-  next();
-}
-
-sequenceStreamer.on('error', function(e, err){
-  console.log(e);
-});
-
-readStream.pipe(characterStreamer).pipe(sequenceStreamer);
-
-
-// readStream.on('open', function () {
-//   // This just pipes the read stream to the response object (which goes to the client)
-//   readStream.pipe(res);
-// });
 
--- test/closest_sequences.js
+++ test/closest_sequences.js
@@ -1,6 +1,58 @@
 var chai = require('chai'),
   expect = chai.expect;
 
+var ClosestSequences = require('../lib/closest_sequences');
+
 describe('ClosestSequences', function(){
-  
+  describe('insert(sequence)', function(){
+    describe('if the maximum number of sequences has not been reached', function(){
+      before(function(){
+        this.closestSequences = new ClosestSequences([0, 0, 0], 3);
+      });
+      it('gets added to the closest sequences', function(){
+        this.closestSequences.insert([1, 1, 1]);
+        this.closestSequences.insert([2, 2, 2]);
+        this.closestSequences.insert([3, 3, 3]);
+
+        expect(this.closestSequences.all()).to eq([
+          [1, 1, 1],
+          [2, 2, 2],
+          [3, 3, 3]
+        ]);
+      });
+    });
+    describe('if the maximum number of sequences has been reached', function(){
+      before(function(){
+        this.closestSequences = new ClosestSequences([0, 0, 0], 3);
+        this.closestSequences.insert([3, 3, 3]);
+        this.closestSequences.insert([6, 6, 6]);
+        this.closestSequences.insert([8, 8, 8]);
+      });
+      describe('if the sequence\'s difference score is greater than or equal to the difference score of the existing sequences', function(){
+        it('does not get added to the closest sequences', function(){
+          this.closestSequences.insert([9, 9, 9]);
+          this.closestSequences.insert([9, 8, 7]);
+
+          expect(this.closestSequences.all()).to eq([
+            [3, 3, 3],
+            [6, 6, 6],
+            [8, 8, 8]
+          ]);
+        });
+      });
+      describe('if the last removed sequence\'s difference score is greater than the difference score of the sequence', function(){
+        it('gets added to the closest sequences', function(){
+
+        });
+        describe('if the number of closest sequences is greater than the max number of close sequences', function(){
+          it('removes the least close sequence (with the highest cloness score)', function(){
+
+          });
+          it('sets the removed sequence\'s difference score to the difference score of the removed sequence', function(){
+
+          });
+        });
+      });
+    });
+  });
 });
