6b28826015b760b9e57284a1b75cf2f1c98173e9
oveddan <stangogh@gmail.com>
2014-05-04 18:26:57 -0400

    refactored - only calculate differnece score up to max, to increase performance

--- test/visual_difference_comparer.js
+++ test/visual_difference_comparer.js
@@ -3,15 +3,15 @@ var chai = require('chai'),
 
 var VisualDifferenceComparer = require('../lib/visual_difference_comparer');
 
-describe('VisualDifferenceComparer', function(){
-  describe('differenceScore(sequence)', function(){
+describe('VisualDifferenceComparer(sequenceToCompareTo)', function(){
+  describe('differenceScore(sequence, maxScore)', function(){
     it('calculates the sum of the differences of each element from the origin sequence', function(){
-      var originalSequence = [0, 3, 2, 9, 5, 4, 0, 8, 3, 6, 2];
-      var toCompareTo =      [1, 3, 0, 7, 6, 4, 4, 5, 2, 8, 6];
+      var sequenceToCompareTo = [0, 3, 2, 9, 5, 4, 0, 8, 3, 6, 2],
+          toCompareTo         = [1, 3, 0, 7, 6, 4, 4, 5, 2, 8, 6],
 
-      var comparer = new VisualDifferenceComparer(originalSequence);
+          comparer = new VisualDifferenceComparer(sequenceToCompareTo);
 
-      var differenceScore = comparer.differenceScore(toCompareTo);
+      var differenceScore = comparer.differenceScore(toCompareTo, null);
 
       var abs = Math.abs;
 
@@ -19,6 +19,16 @@ describe('VisualDifferenceComparer', function(){
         (4 - 4) + (4 - 0) + abs(5 - 8) + abs(2 - 3) + (8 - 6) + (6 - 2);
 
       expect(differenceScore).to.eq(expectedResult);
+    });
+    describe('stops calculating when it reaches the max score, and returns infinity', function(){
+      var originalSequence = [1, 1, 1];
+          toCompareTo =      [1, 3, 1];
+
+          comparer = new VisualDifferenceComparer(originalSequence);
+
+      var differenceScore = comparer.differenceScore(toCompareTo, 2);
+
+      expect(differenceScore).to.eq(Number.POSITIVE_INFINITY);
     });
   });
 });
