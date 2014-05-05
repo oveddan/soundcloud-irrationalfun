110512861fe260a78f17f6ad43c9583e778bfd2a
oveddan <stangogh@gmail.com>
2014-05-05 03:35:14 -0400

    refactored - renamed multi core sequences finder

--- test/closest_sequences_finder.js
+++ test/closest_sequences_finder.js
@@ -1,7 +1,7 @@
 var chai = require('chai'),
   expect = chai.expect;
 
-var ClosestSequences = require('../lib/closest_sequences');
+var ClosestSequencesFinder = require('../lib/closest_sequences_finder');
 
 var sequenceNodeComparer = function(a, b){
   if(a.differenceScore < b.differenceScore) 
@@ -10,18 +10,18 @@ var sequenceNodeComparer = function(a, b){
     return 1;
 }
 
-describe('ClosestSequences', function(){
+describe('ClosestSequencesFinder', function(){
   describe('insert(sequence)', function(){
     describe('if the maximum number of sequences has not been reached', function(){
       beforeEach(function(){
-        this.closestSequences = new ClosestSequences([0, 0, 0], 3);
+        this.closestSequencesFinder = new ClosestSequencesFinder([0, 0, 0], 3);
       });
       it('gets added to the closest sequences', function(){
-        this.closestSequences.insert([1, 1, 1], 3);
-        this.closestSequences.insert([2, 2, 2], 5);
-        this.closestSequences.insert([3, 3, 3], 7);
+        this.closestSequencesFinder.insert([1, 1, 1], 3);
+        this.closestSequencesFinder.insert([2, 2, 2], 5);
+        this.closestSequencesFinder.insert([3, 3, 3], 7);
 
-        expect(this.closestSequences.all()).to.eql([
+        expect(this.closestSequencesFinder.all()).to.eql([
           {
             sequence : [1, 1, 1],
             differenceScore : 3,
@@ -42,17 +42,17 @@ describe('ClosestSequences', function(){
     });
     describe('if the maximum number of sequences has been reached', function(){
       beforeEach(function(){
-        this.closestSequences = new ClosestSequences([0, 0, 0], 3);
-        this.closestSequences.insert([6, 6, 6], 5);
-        this.closestSequences.insert([3, 3, 3], 2);
-        this.closestSequences.insert([8, 8, 8], 27);
+        this.closestSequencesFinder = new ClosestSequencesFinder([0, 0, 0], 3);
+        this.closestSequencesFinder.insert([6, 6, 6], 5);
+        this.closestSequencesFinder.insert([3, 3, 3], 2);
+        this.closestSequencesFinder.insert([8, 8, 8], 27);
       });
       describe('if the sequence\'s difference score is greater than or equal to the difference score of the existing sequences', function(){
         it('does not get added to the closest sequences', function(){
-          this.closestSequences.insert([7, 8, 9], 30);
-          this.closestSequences.insert([9, 9, 9], 10);
+          this.closestSequencesFinder.insert([7, 8, 9], 30);
+          this.closestSequencesFinder.insert([9, 9, 9], 10);
 
-          expect(this.closestSequences.all()).to.eql([
+          expect(this.closestSequencesFinder.all()).to.eql([
             {
               sequence : [3, 3, 3],
               differenceScore : 9,
@@ -73,10 +73,10 @@ describe('ClosestSequences', function(){
       });
       describe('if the sequence\'s difference score is less than the difference scores of any of the existing sequences', function(){
         it('gets added to the closest sequences and removes the element with the highest difference score', function(){
-          this.closestSequences.insert([1, 1, 1], 900);
-          this.closestSequences.insert([7, 7, 7], 360);
+          this.closestSequencesFinder.insert([1, 1, 1], 900);
+          this.closestSequencesFinder.insert([7, 7, 7], 360);
 
-          expect(this.closestSequences.all()).to.eql([
+          expect(this.closestSequencesFinder.all()).to.eql([
             {
               sequence : [1, 1, 1],
               differenceScore : 3,
