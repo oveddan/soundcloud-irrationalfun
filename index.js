cd1378134ac2392efdb58640512f2d6c1c44e5d4
oveddan <stangogh@gmail.com>
2014-05-05 00:24:00 -0400

    getting this to work without express sucks, especially with templating

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-var multiCoreClosestPiSequencesFinder = require('./lib/multi_core_closest_pi_sequences_finder');
+var fs = require('fs'),
+  util = require('util');
 
-var numDigits = 1000000;
+var multiCoreClosestPiSequencesFinder = require('./lib/multi_core_closest_pi_sequences_finder'),
+  soundcloud = require('./lib/soundcloud');
+
+var numDigits = 10000000;
+var numCPUs = require('os').cpus().length;
 
 var start = new Date();
 
-multiCoreClosestPiSequencesFinder.findUsingMultipleCores(1, numDigits, function(err, results){    
-  console.log('for num digits ' + numDigits);
+multiCoreClosestPiSequencesFinder.findUsingMultipleCores(numCPUs, numDigits, function(err, results){    
   var end = new Date();
-  console.log('time elapsed ' + (end - start) + 'ms');
-  console.log('results:');
+  var timeElapsed = end - start;
+
+  var resultsForView = convertForView(timeElapsed, results);
   
+  populateResultsIntoHtmlFile(resultsForView);
+});
+
+var convertForView = function(timeElapsed, results) {
   var sortedResults = results.sort(function(a, b){
     if(a.differenceScore < b.differenceScore)
       return -1;
     else
       return 1;
   });
-  
-  console.log(sortedResults);
-});
+
+  var resultsWithSequencesAsGrid = sortedResults.map(function(result){
+    return {
+      sequenceGrid : soundcloud.convertToHexGrid(result.sequence),
+      offset : result.offset
+    };
+  });
+
+  return {
+    timeElapsed : timeElapsed,
+    results : resultsWithSequencesAsGrid
+  };
+};
+
+var populateResultsIntoHtmlFile = function(model) {
+  fs.readFile('results_viewer.html.template', function(err, templateContents){
+    // todo: use an actual templating engine
+    var html = templateContents.toString().
+      replace("{results}", util.inspect(model.results)).
+      replace("{timeElapsed}", model.timeElapsed);
+
+    fs.writeFile('results_viewer.html', html, function(err, result){
+      console.log('Closest sequences written to results_viewer.html');
+    });
+  });
+}
 
 
 
